3e37cc7e0adc3a14a08539f4732142e8636c6996
Pasha <PashaBBibko@gmail.com>
2026-01-24 19:49:33 +0000

    Added basic code profiler

---
+++ Assets/Pacore/Shared/DevTools/CodeProfiler.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System;
+
+namespace PashaBibko.Pacore.Shared.DevTools
+{
+    public static class CodeProfiler
+    {
+        private static Dictionary<string, List<long>> ProfilerSnippets { get; } = new();
+        public static IReadOnlyDictionary<string, List<long>> GetProfilerSnippets() => ProfilerSnippets;
+        
+        private static void AddProfileSnippet(string name, long ms)
+        {
+            if (!ProfilerSnippets.ContainsKey(name))
+            {
+                ProfilerSnippets.Add(name, new List<long>());
+            }
+            
+            ProfilerSnippets[name].Add(ms);
+        }
+
+        public static ProfilerSnippetHandle Start(string name) => new(name);
+
+        public class ProfilerSnippetHandle : IDisposable
+        {
+            private Stopwatch Stopwatch { get; }
+            private string SnippetName { get; }
+            
+            public ProfilerSnippetHandle(string name)
+            {
+                Stopwatch = Stopwatch.StartNew();
+                SnippetName = name;
+            }
+
+            public void Dispose()
+            {
+                Stopwatch.Stop();
+                AddProfileSnippet(SnippetName, Stopwatch.ElapsedMilliseconds);
+            }
+        }
+    }
+}
